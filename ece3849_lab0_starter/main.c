801e754155b169a3e05e008a901d1e97fd2e123f
Nguyen <hpnguyen@wpi.edu>
2018-03-14 13:56:32 -0400

    wow I'm getting paranoid just reading through the lab right now: a bunch of codes is thrown at my face with an addtional 5 datasheets to read through... Please be gentle on me... Anyway, got the button and ISR setup with supposedly correct format for the LCD output. I will test and finish the rest on the actual board. Also figured out how to fix the compiling error on windows, just missing some external libraries and invalid path variable

--- ece3849_lab0_starter/main.c
+++ ece3849_lab0_starter/main.c
@@ -8,6 +8,7 @@
  *
  */
 
+// Some existed library
 #include <stdint.h>
 #include <stdbool.h>
 #include "driverlib/fpu.h"
@@ -15,6 +16,9 @@
 #include "driverlib/interrupt.h"
 #include "Crystalfontz128x128_ST7735.h"
 #include <stdio.h>
+
+// Button for handling debounce and ISR
+#include "buttons.h"
 
 uint32_t gSystemClock; // [Hz] system clock frequency
 volatile uint32_t gTime = 8345; // time in hundredths of a second
@@ -42,11 +46,15 @@ int main(void)
     // full-screen rectangle
     tRectangle rectFullScreen = {0, 0, GrContextDpyWidthGet(&sContext)-1, GrContextDpyHeightGet(&sContext)-1};
 
+    // LCD init done, move on to ISR
+    ButtonInit(); // setup the ISR for our counter
+    IntMasterEnable; // now that we finished setting things up, re-enable interrupts
+
     while (true) {
         GrContextForegroundSet(&sContext, ClrBlack);
         GrRectFill(&sContext, &rectFullScreen); // fill screen with black
         time = gTime; // read shared global only once
-        snprintf(str, sizeof(str), "Time = %06u", time); // convert time to string
+        snprintf(str, sizeof(str), "Time = %02u:%02u:%02u", (time/3600), ((time/60)%60), (time%60)); // convert time to string
         GrContextForegroundSet(&sContext, ClrYellow); // yellow text
         GrStringDraw(&sContext, str, /*length*/ -1, /*x*/ 0, /*y*/ 0, /*opaque*/ false);
         GrFlush(&sContext); // flush the frame buffer to the LCD
