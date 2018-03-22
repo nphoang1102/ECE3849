702f9afc2a420d2b373082c191c1b0f1070c4e24
Hoang Nguyen <nphoang1102@gmail.com>
2018-03-22 12:58:35 -0400

    setting up the ADC, initializing and working on ISR

--- ece3849d18_lab1_hpnguyen_slee2/adc.c
+++ ece3849d18_lab1_hpnguyen_slee2/adc.c
@@ -18,6 +18,7 @@
 #include "driverlib/interrupt.h"
 #include "driverlib/adc.h"
 #include "sysctl_pll.h"
+#include "adc.h"
 
 // Initialize ADC1 for oscillator
 void ADCinit(void) {
@@ -26,20 +27,39 @@ void ADCinit(void) {
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
     GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_0); // GPIO setup pin E0
 
-
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0); // initialize ADC peripherals
+    // Clock configuration for ADC1
     SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC1);
-    // ADC clock
     uint32_t pll_frequency = SysCtlFrequencyGet(CRYSTAL_FREQUENCY);
     uint32_t pll_divisor = (pll_frequency - 1) / (16 * ADC_SAMPLING_RATE) + 1; //round up
-    ADCClockConfigSet(ADC0_BASE, ADC_CLOCK_SRC_PLL | ADC_CLOCK_RATE_FULL, pll_divisor);
     ADCClockConfigSet(ADC1_BASE, ADC_CLOCK_SRC_PLL | ADC_CLOCK_RATE_FULL, pll_divisor);
-    ADCSequenceDisable(...); // choose ADC1 sequence 0; disable before configuring
-    ADCSequenceConfigure(...); // specify the "Always" trigger
-    ADCSequenceStepConfigure(...);// in the 0th step, sample channel 3 (AIN3)
+
+    // Initialize ADC1 peripheral
+    ADCSequenceDisable(ADC1_BASE, 0); // choose ADC1 sequence 0; disable before configuring
+    ADCSequenceConfigure(ADC1_BASE, 0, ADC_TRIGGER_ALWAYS, 0); // specify the "Always" trigger
+    ADCSequenceStepConfigure(ADC1_BASE, 0, 0, ADC_CTL_CH3 | ADC_CTL_IE | ADC_CTL_END);// in the 0th step, sample channel 3 (AIN3)
      // enable interrupt, and make it the end of sequence
-    ADCSequenceEnable(...); // enable the sequence. it is now sampling
-    ADCIntEnable(...); // enable sequence 0 interrupt in the ADC1 peripheral
-    IntPrioritySet(...); // set ADC1 sequence 0 interrupt priority
-    IntEnable(...); // enable ADC1 sequence 0 interrupt in int. controller
+    
+    // Hook up the ISR
+    IntPrioritySet(INT_ADC1SS0, 29); // set ADC1 sequence 0 interrupt priority
+    ADCIntEnable(ADC1_BASE, 0); // enable sequence 0 interrupt in the ADC1 peripheral
+    
+    // Fire the interrupt and sequence now
+    IntEnable(INT_ADC1SS0); // enable ADC1 sequence 0 interrupt in int. controller
+    ADCSequenceEnable(ADC1_BASE, 0); // enable the sequence. it is now sampling
+}
+
+
+// ISR for ADC1
+void ADC_ISR(void) {
+    // First thing first, clear the interrupt flag so we can exit
+
+
+    if (ADC1_OSTAT_R & ADC_OSTAT_OV0) { // check for ADC FIFO overflow
+         gADCErrors++; // count errors
+         ADC1_OSTAT_R = ADC_OSTAT_OV0; // clear overflow condition
+     }
+     gADCBuffer[
+         gADCBufferIndex = ADC_BUFFER_WRAP(gADCBufferIndex + 1)
+     ] = <...>; // read sample from the ADC1 sequence 0 FIFO
+
 }
--- ece3849d18_lab1_hpnguyen_slee2/adc.h
+++ ece3849d18_lab1_hpnguyen_slee2/adc.h
@@ -13,4 +13,16 @@
 
 #include <stdint.h>
 
+// Parameters for clock setup
+#define ADC_SAMPLING_RATE 1000000   // [samples/sec] desired ADC sampling rate
+#define CRYSTAL_FREQUENCY 25000000 
+
+// Prameters for ISR
+#define ADC_INT_PRIORITY 0
+
+// initialize ADC1 for data sampling
+void ADCinit(void);
+
+// 
+
 #endif /* ADC_H_ */
