23a03e22a26857a51d5b08d4ab2055631b1944aa
Hoang Nguyen <nphoang1102@gmail.com>
2018-04-07 17:43:21 -0400

    added the hardware initialization code into main.c, then ended up getting the same issue as the ISR error in lab1...

--- ece3849_lab2_hpnguyen_slee2/main.c
+++ ece3849_lab2_hpnguyen_slee2/main.c
@@ -6,15 +6,25 @@
 
 /* Importing header files */
 
+// Some existed library
+#include <stdint.h>
+#include <stdbool.h>
+#include "driverlib/fpu.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "Crystalfontz128x128_ST7735.h"
+#include <stdio.h>
+
 // Import local header file
+#include "adc.h"
+#include "buttons.h"
+#include "lcd_display.h"
 #include "RTOS_helper.h"
+#include "timer.h"
 
-
+// Global variable
 uint32_t gSystemClock = 120000000; // [Hz] system clock frequency
 
-void task0_func(UArg arg1, UArg arg2);
-void ButtonClockSignal(void);
-void ButtonTask(UArg arg0, UArg arg1);
 
 /*
  *  ======== main ========
@@ -23,7 +33,19 @@ int main(void)
 {
     IntMasterDisable();
 
-    // hardware initialization goes here
+    /* Hardware initialization goes here */
+
+    // Enable the Floating Point Unit, and permit ISRs to use it
+    FPUEnable();
+    FPULazyStackingEnable();
+
+    // Initialize the system clock to 120 MHz
+    gSystemClock = SysCtlClockFreqSet(SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480, 120000000);
+
+    // Initialization here
+    lcd_init();
+    ButtonInit(); // setup the ISR for our counter, will not need under RTOS anymore
+    ADCinit();
 
     /* Start BIOS */
     BIOS_start();
